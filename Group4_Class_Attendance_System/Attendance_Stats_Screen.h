3d24e751add72d90b315513843cb76babdc56c91
KeshavJ <keshav.jeewanlall@gmail.com>
2016-10-13 20:42:22 +0200

    added DB connection

--- Group4_Class_Attendance_System/Attendance_Stats_Screen.h
+++ Group4_Class_Attendance_System/Attendance_Stats_Screen.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 namespace Group4_Class_Attendance_System {
 
 	using namespace System;
@@ -8,6 +9,8 @@ namespace Group4_Class_Attendance_System {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
+	
 
 	/// <summary>
 	/// Summary for Attendance_Stats_Screen
@@ -34,6 +37,10 @@ namespace Group4_Class_Attendance_System {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  textBox1;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label1;
 
 	private:
 		/// <summary>
@@ -48,18 +55,50 @@ namespace Group4_Class_Attendance_System {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(124, 59);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(162, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(124, 109);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Search";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(26, 62);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(92, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Enter Student No.";
 			// 
 			// Attendance_Stats_Screen
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(323, 272);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
 			this->Name = L"Attendance_Stats_Screen";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Attendance_Stats_Screen";
 			this->Load += gcnew System::EventHandler(this, &Attendance_Stats_Screen::Attendance_Stats_Screen_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -67,6 +106,18 @@ namespace Group4_Class_Attendance_System {
 
 	private: System::Void Attendance_Stats_Screen_Load(System::Object^  sender, System::EventArgs^  e) {
 		//This method will run when screen is loaded so put connection to DB here
+		String^ constring = L"datasource=127.0.0.1; port=3306; username=root; password=keshav";
+		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+		MySqlCommand^ cmdDataBase = gcnew MySqlCommand("select * from studentattendancedb.studentattendancetbl;", conDataBase);
+		MySqlDataReader^ myReader;
+
+		try{
+			conDataBase->Open();
+			myReader = cmdDataBase->ExecuteReader();
+		} catch (Exception^ ex){
+
+			MessageBox::Show(ex->Message);
+		}
 
 		//Make a textbox and button. User will enter student number in the textbox. When button pressed, read the student 
 		//number from the textbox and get data for that student from DB. So put the other code in the "button pressed" method.
